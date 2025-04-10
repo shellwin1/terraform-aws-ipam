a15d137958101917a9045d00b38f87624ce5ffed
Łukasz Skorupka <skorupka813@gmail.com>
2025-04-10 08:36:56 +0200

    Temp

--- examples/single_scope_ipv4/main.tf
+++ examples/single_scope_ipv4/main.tf
@@ -13,67 +13,15 @@ module "basic" {
 
   pool_configurations = {
     corporate-us-west-2 = {
-      description = "2nd level, locale us-west-2 pool"
+      description = "2nd level, locale eu-central-1 pool"
       cidr        = ["10.0.0.0/16", "10.1.0.0/16"]
 
       sub_pools = {
 
-        sandbox = {
-          name                 = "mysandbox"
-          cidr                 = ["10.0.0.0/20"]
-          ram_share_principals = var.sandbox_ou_arn
-          allocation_resource_tags = {
-            env = "sandbox"
-          }
-        }
         dev = {
           netmask_length = 20
-
-          sub_pools = {
-            team_a = {
-              netmask_length       = 24
-              ram_share_principals = var.prod_account # prod account
-              locale               = "us-west-2"
-            }
-
-            team_b = {
-              netmask_length       = 26
-              ram_share_principals = var.prod_account # prod account
-            }
-          }
-        }
-        prod = {
-          cidr   = ["10.1.16.0/20"]
-          locale = "us-west-2"
-
-          sub_pools = {
-            team_a = {
-              cidr                 = ["10.1.16.0/24"]
-              ram_share_principals = var.prod_account # prod account
-            }
-
-            team_b = {
-              cidr                 = ["10.1.17.0/24"]
-              ram_share_principals = var.prod_account # prod account
-            }
-          }
-        }
-      }
-    }
-    us-east-1 = {
-      cidr   = ["10.2.0.0/16"]
-      locale = "us-east-1"
-
-      sub_pools = {
-
-        team_a = {
-          cidr                 = ["10.2.0.0/20"]
-          ram_share_principals = var.prod_ou_arn
-        }
-
-        team_b = {
-          cidr                 = ["10.2.16.0/20"]
-          ram_share_principals = var.prod_ou_arn
+          ram_share_principals = var.prod_account
+          locale = "eu-central-1"
         }
       }
     }
--- examples/single_scope_ipv4/variables.tf
+++ examples/single_scope_ipv4/variables.tf
@@ -1,17 +1,5 @@
 variable "prod_account" {
   description = "Used for testing, prod account id"
   type        = list(string)
-  default     = null
-}
-
-variable "prod_ou_arn" {
-  description = "arn of ou to share to prod accounts"
-  type        = list(string)
-  default     = null
-}
-
-variable "sandbox_ou_arn" {
-  description = "arn of ou to share to sandbox accounts"
-  type        = list(string)
-  default     = null
+  default     = ["653584809638"]
 }
